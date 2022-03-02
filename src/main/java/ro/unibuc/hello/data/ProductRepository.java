cbdf6486a7c32f4f6b313cfeef588d78395a95e8
Vikcoc <vikcoc@yahoo.com>
2022-03-02 04:34:51 -0500

    The get product feature

--- src/main/java/ro/unibuc/hello/data/ProductRepository.java
+++ src/main/java/ro/unibuc/hello/data/ProductRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends MongoRepository<ProductEntity, String> {
 
-    InformationEntity findByTitle(String title);
-    List<InformationEntity> findByDescription(String description);
+    ProductEntity findByTitle(String title);
+    List<ProductEntity> findByDescription(String description);
 }
