ff7a917732467ed06a39dbbb6cdfb30eb282a578
CalciuAndrei <andrei.calciu@s.unibuc.ro>
2022-03-03 13:59:08 +0200

    Added Relationships for API on Movie, Review.

--- src/main/java/ro/unibuc/hello/controller/MovieController.java
+++ src/main/java/ro/unibuc/hello/controller/MovieController.java
@@ -7,8 +7,7 @@ import java.util.List;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import ro.unibuc.hello.data.MovieRepository;
-import ro.unibuc.hello.data.MovieEntity;
+import ro.unibuc.hello.data.*;
 import ro.unibuc.hello.dto.MovieDTO;
 
 @RestController
@@ -17,18 +16,26 @@ public class MovieController {
     @Autowired
     private MovieRepository movieRepository;
 
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Autowired
+    private WatchItemRepository watchItemRepository;
+
     @GetMapping("/movie/getAll")
     @ResponseBody
-    public List<MovieDTO> getMovies(){
+    public List<MovieDTO> getMovies() {
         ArrayList<MovieDTO>movieDTOs = new ArrayList<>();
+
         movieRepository.findAll().forEach(movieEntity -> movieDTOs.add(new MovieDTO(movieEntity)));
         return movieDTOs;
     }
 
     @GetMapping("/movie/get")
     @ResponseBody
-    public MovieDTO getMovie(@RequestParam(name="id")String id){
+    public MovieDTO getMovie(@RequestParam(name="id") String id) {
         MovieEntity movie = movieRepository.findById(String.valueOf(new ObjectId(id))).orElse(null);
+
         if (movie!=null)
             return new MovieDTO(movie);
         else
@@ -37,22 +44,47 @@ public class MovieController {
 
     @PostMapping("/movie/insert")
     @ResponseBody
-    public MovieEntity insertMovie(@RequestParam(name="title")String title,@RequestParam(name="director")String director,
-                                   @RequestParam(name="writer")String writer,@RequestParam(name="year")Integer year,
-                                   @RequestParam(name="duration")Integer duration)
-    {
-        return movieRepository.save(new MovieEntity(title,director,writer,year,duration));
+    public MovieDTO insertMovie(@RequestParam(name="title") String title,
+                                @RequestParam(name="director") String director,
+                                @RequestParam(name="writer") String writer,
+                                @RequestParam(name="year") Integer year,
+                                @RequestParam(name="duration") Integer duration,
+                                @RequestParam(name="reviewIds") List<String>reviewIds,
+                                @RequestParam(name="watchItemIds") List<String>watchItemIds) {
+        MovieEntity movie=new MovieEntity(title,director,writer,year,duration);
+        ArrayList<ReviewEntity> reviewEntities = new ArrayList<>();
+        ArrayList<WatchItemEntity> watchItemEntities = new ArrayList<>();
+
+        if(!reviewIds.isEmpty())
+            reviewIds.forEach(id -> reviewEntities.add(reviewRepository.findById(String.valueOf(new ObjectId(id))).orElse(null)));
+        else
+            movie.setReviews(null);
+
+        if(!watchItemIds.isEmpty())
+            watchItemIds.forEach(id -> watchItemEntities.add(watchItemRepository.findById(String.valueOf(new ObjectId(id))).orElse(null)));
+        else
+            movie.setWatchItems(null);
+
+        if(!reviewEntities.isEmpty())
+            movie.setReviews(reviewEntities);
+
+        if(!watchItemEntities.isEmpty())
+            movie.setWatchItems(watchItemEntities);
+        return new MovieDTO(movieRepository.save(movie));
     }
 
     @PutMapping("/movie/update")
     @ResponseBody
-    public MovieDTO updateMovie(@RequestParam(name="id")String id,
-                                @RequestParam(name="title",required = false)String title,
-                                @RequestParam(name="director",required = false)String director,
-                                @RequestParam(name="writer",required = false)String writer,
-                                @RequestParam(name="year",required = false)Integer year,
-                                @RequestParam(name="duration",required = false)Integer duration){
+    public MovieDTO updateMovie(@RequestParam(name="id") String id,
+                                @RequestParam(name="title",required = false) String title,
+                                @RequestParam(name="director",required = false) String director,
+                                @RequestParam(name="writer",required = false) String writer,
+                                @RequestParam(name="year",required = false) Integer year,
+                                @RequestParam(name="duration",required = false) Integer duration,
+                                @RequestParam(name="reviewIds") List<String> reviewIds,
+                                @RequestParam(name="watchItemIds") List<String> watchItemIds) {
         MovieEntity movie=movieRepository.findById(String.valueOf(new ObjectId(id))).orElse(null);
+
         if(movie!=null)
         {
             if(title!=null)
@@ -65,6 +97,24 @@ public class MovieController {
                 movie.setYear(year);
             if(duration!=null)
                 movie.setDuration(duration);
+
+            if(!reviewIds.isEmpty()) {
+                ArrayList<ReviewEntity> reviewEntities = new ArrayList<>();
+                reviewIds.forEach(reviewId -> reviewEntities.add(reviewRepository.findById(String.valueOf(new ObjectId(reviewId))).orElse(null)));
+                if(!reviewEntities.isEmpty())
+                    movie.setReviews(reviewEntities);
+            }
+            else
+                movie.setReviews(null);
+
+            if(!watchItemIds.isEmpty()) {
+                ArrayList<WatchItemEntity> watchItemEntities = new ArrayList<>();
+                watchItemIds.forEach(watchItemId -> watchItemEntities.add(watchItemRepository.findById(String.valueOf(new ObjectId(watchItemId))).orElse(null)));
+                if(!watchItemEntities.isEmpty())
+                    movie.setWatchItems(watchItemEntities);
+            }
+            else
+                movie.setWatchItems(null);
             return new MovieDTO(movieRepository.save(movie));
         }
         else return null;
@@ -72,8 +122,7 @@ public class MovieController {
 
     @DeleteMapping("/movie/delete")
     @ResponseBody
-    public void deleteReview(@RequestParam(name="id")String id){
-
+    public void deleteReview(@RequestParam(name="id") String id) {
         movieRepository.deleteById(String.valueOf(new ObjectId(id)));
     }
 }
--- src/main/java/ro/unibuc/hello/controller/ReviewController.java
+++ src/main/java/ro/unibuc/hello/controller/ReviewController.java
@@ -6,8 +6,7 @@ import java.util.List;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import ro.unibuc.hello.data.ReviewRepository;
-import ro.unibuc.hello.data.ReviewEntity;
+import ro.unibuc.hello.data.*;
 import ro.unibuc.hello.dto.ReviewDTO;
 
 @RestController
@@ -16,11 +15,17 @@ public class ReviewController {
     @Autowired
     private ReviewRepository reviewRepository;
 
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/review/getAll")
     @ResponseBody
-    public List<ReviewDTO> getReviews(){
-
+    public List<ReviewDTO> getReviews() {
         ArrayList<ReviewDTO> reviewDTOs = new ArrayList<>();
+
         reviewRepository.findAll().forEach(reviewEntity -> reviewDTOs.add(new ReviewDTO(reviewEntity)));
         return reviewDTOs;
     }
@@ -29,6 +34,7 @@ public class ReviewController {
     @ResponseBody
     public ReviewDTO getReview(@RequestParam (name="id") String id) {
         ReviewEntity review = reviewRepository.findById(String.valueOf(new ObjectId(id))).orElse(null);
+
         if(review != null)
             return new ReviewDTO(review);
         else
@@ -37,23 +43,58 @@ public class ReviewController {
 
     @PostMapping("/review/insert")
     @ResponseBody
-    public ReviewEntity insertReview(@RequestParam(name="comment")String comment,
-                                     @RequestParam(name="score")Integer score){
-        return reviewRepository.save(new ReviewEntity(comment,score));
+    public ReviewDTO insertReview(@RequestParam(name="comment") String comment,
+                                     @RequestParam(name="score") Integer score,
+                                     @RequestParam(name="movieId") String movieId,
+                                     @RequestParam(name="userId") String userId) {
+        ReviewEntity review =new ReviewEntity(comment,score);
+
+        if(movieId != null) {
+            MovieEntity movie = movieRepository.findById(String.valueOf(new ObjectId(movieId))).orElse(null);
+
+            review.setMovie(movie);
+        }
+        else
+            return null;
+
+        if(userId != null) {
+            UserEntity user = userRepository.findById(String.valueOf(new ObjectId(userId))).orElse(null);
+
+            review.setUser(user);
+        }
+        else
+            return null;
+        return new ReviewDTO(reviewRepository.save(review));
     }
 
     @PutMapping("/review/update")
     @ResponseBody
-    public ReviewDTO updateReview(@RequestParam(name="id")String id,
-                                  @RequestParam(name="comment",required = false)String comment,
-                                  @RequestParam(name="score",required = false)Integer score)
+    public ReviewDTO updateReview(@RequestParam(name="id") String id,
+                                  @RequestParam(name="comment",required = false) String comment,
+                                  @RequestParam(name="score",required = false) Integer score,
+                                  @RequestParam(name="movieId",required = false) String movieId,
+                                  @RequestParam(name="userId", required = false) String userId)
     {
         ReviewEntity review=reviewRepository.findById(String.valueOf(new ObjectId(id))).orElse(null);
+
         if(review!=null) {
             if (comment != null)
                 review.setComment(comment);
             if (score != null)
                 review.setScore(score);
+            if(movieId != null) {
+                MovieEntity movie = movieRepository.findById(String.valueOf(new ObjectId(movieId))).orElse(null);
+
+                if(movie != null)
+                    review.setMovie(movie);
+            }
+
+            if(userId != null) {
+                UserEntity user= userRepository.findById(String.valueOf(new ObjectId(userId))).orElse(null);
+
+                if(user != null)
+                    review.setUser(user);
+            }
             return new ReviewDTO(reviewRepository.save(review));
         }
         else
@@ -62,7 +103,7 @@ public class ReviewController {
 
     @DeleteMapping("/review/delete")
     @ResponseBody
-    public void deleteReview(@RequestParam(name="id")String id){
+    public void deleteReview(@RequestParam(name="id") String id){
         reviewRepository.deleteById(String.valueOf(new ObjectId(id)));
     }
 }
