377d2435b9c7dc62125d565807449c4178da2024
Răzvan Burlăcioiu <burlacio@adobe.com>
2022-03-05 15:57:36 +0200

    Enable prom endpoint for monitoring

--- src/main/java/ro/unibuc/hello/controller/HelloWorldController.java
+++ src/main/java/ro/unibuc/hello/controller/HelloWorldController.java
@@ -1,7 +1,8 @@
 package ro.unibuc.hello.controller;
 
-import java.util.concurrent.atomic.AtomicLong;
-
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +12,16 @@ import ro.unibuc.hello.data.InformationEntity;
 import ro.unibuc.hello.data.InformationRepository;
 import ro.unibuc.hello.dto.Greeting;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @Controller
 public class HelloWorldController {
 
     @Autowired
     private InformationRepository informationRepository;
+
+    @Autowired
+    MeterRegistry metricsRegistry;
 
     private static final String helloTemplate = "Hello, %s!";
     private static final String informationTemplate = "%s : %s!";
@@ -23,12 +29,17 @@ public class HelloWorldController {
 
     @GetMapping("/hello-world")
     @ResponseBody
+    @Timed(value = "hello.greeting.time", description = "Time taken to return greeting")
+    @Counted(value = "hello.greeting.count", description = "Times greeting was returned")
     public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
+        metricsRegistry.counter("my_non_aop_metric", "endpoint", "hello").increment(counter.incrementAndGet());
         return new Greeting(counter.incrementAndGet(), String.format(helloTemplate, name));
     }
 
     @GetMapping("/info")
     @ResponseBody
+    @Timed(value = "hello.info.time", description = "Time taken to return info")
+    @Counted(value = "hello.info.count", description = "Times info was returned")
     public Greeting listAll(@RequestParam(name="title", required=false, defaultValue="Overview") String title) {
         InformationEntity entity = informationRepository.findByTitle(title);
         return new Greeting(counter.incrementAndGet(), String.format(informationTemplate, entity.title, entity.description));
