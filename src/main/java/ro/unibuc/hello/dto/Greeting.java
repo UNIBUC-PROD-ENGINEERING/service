450565d2a84d6f8395f5515cd31a2e9d2d37ad62
butan <butan@adobe.com>
2022-03-13 23:08:12 +0200

    [Lab 5] - adding E2E test setup and example

--- src/main/java/ro/unibuc/hello/dto/Greeting.java
+++ src/main/java/ro/unibuc/hello/dto/Greeting.java
@@ -2,11 +2,22 @@ package ro.unibuc.hello.dto;
 
 public class Greeting {
 
-    private final long id;
-    private final String content;
+    private long id;
+    private String content;
+
+    public Greeting() {
+    }
 
     public Greeting(long id, String content) {
         this.id = id;
+        this.content = content;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setContent(String content) {
         this.content = content;
     }
 
---
+++ src/test/java/ro/unibuc/hello/e2e/HelloWorldE2E.java
@@ -0,0 +1,10 @@
+package ro.unibuc.hello.e2e;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources")
+public class HelloWorldE2E {
+}
