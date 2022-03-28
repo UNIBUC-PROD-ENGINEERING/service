42aac833bd904d31e2e812678a6c9923a8765887
butan <butan@adobe.com>
2022-03-28 12:48:52 +0300

    added separate gradle tasks for unit, IT, E2E tests

--- src/test/java/ro/unibuc/hello/e2e/steps/HelloWorldSteps.java
+++ src/test/java/ro/unibuc/hello/e2e/steps/HelloWorldSteps.java
@@ -3,23 +3,19 @@ package ro.unibuc.hello.e2e.steps;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.spring.CucumberContextConfiguration;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.RestTemplate;
-
 import ro.unibuc.hello.dto.Greeting;
 import ro.unibuc.hello.e2e.util.HeaderSetup;
 import ro.unibuc.hello.e2e.util.ResponseErrorHandler;
 import ro.unibuc.hello.e2e.util.ResponseResults;
-
 
 import java.util.HashMap;
 import java.util.Map;
--- src/test/java/ro/unibuc/hello/service/HelloWorldServiceTestIT.java
+++ src/test/java/ro/unibuc/hello/service/HelloWorldServiceTestIT.java
@@ -1,15 +1,15 @@
 package ro.unibuc.hello.service;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import ro.unibuc.hello.data.InformationRepository;
 import ro.unibuc.hello.dto.Greeting;
 
 @SpringBootTest
+@Tag("IT")
 class HelloWorldServiceTestIT {
 
     @Autowired
