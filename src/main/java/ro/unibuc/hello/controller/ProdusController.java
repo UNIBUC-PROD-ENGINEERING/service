5fab0c11e9093b47dd8fa2eb9fb49cdb8e9f7122
Ruxandra Oprea <ruxandranaoprea@yahoo.com>
2023-03-10 00:32:01 +0200

    added getProdus, createProdus and getAllProduse

---
+++ src/main/java/ro/unibuc/hello/controller/ProdusController.java
@@ -0,0 +1,40 @@
+package ro.unibuc.hello.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import ro.unibuc.hello.dto.Greeting;
+import ro.unibuc.hello.dto.ProdusDTO;
+import ro.unibuc.hello.exception.EntityNotFoundException;
+import ro.unibuc.hello.service.ProdusService;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class ProdusController {
+
+    @Autowired
+    private ProdusService produsService;
+
+    @GetMapping("/getProdus")
+    @ResponseBody
+    public Optional<ProdusDTO> getProdus(@RequestParam(name="id", required=false) String id) {
+        return produsService.getProdus(id);
+    }
+    @PostMapping("/createProdus")
+    @ResponseBody
+    public void createProdus( ProdusDTO produs) {
+         produsService.createProdus(produs);
+    }
+    @GetMapping("/getAllProduse")
+    @ResponseBody
+    public List<ProdusDTO> getAll() {
+        return produsService.getAll();
+    }
+
+
+}
