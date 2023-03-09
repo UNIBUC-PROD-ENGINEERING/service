bd2972bf713bb02cd31bd18f8cf357a28c5886f6
Ana <anamares10@gmail.com>
2023-03-09 18:00:56 +0200

    Add AvionRepository with find (Read) function

---
+++ src/main/java/ro/unibuc/hello/data/AvionRepository.java
@@ -0,0 +1,17 @@
+package ro.unibuc.hello.data;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * No need to implement this interface.
+ * Spring Data MongoDB automatically creates a class it implementing the interface when you run the application.
+ */
+@Repository
+public interface AvionRepository extends MongoRepository<Avion, String> {
+
+    Avion findByNumber(String number);
+
+}
