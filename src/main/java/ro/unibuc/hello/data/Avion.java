64085ca8dce7c7313d445de2f3675b9cda5ab3a1
Ana <anamares10@gmail.com>
2023-03-09 18:36:59 +0200

    Add getters and setters to Avion

--- src/main/java/ro/unibuc/hello/data/Avion.java
+++ src/main/java/ro/unibuc/hello/data/Avion.java
@@ -18,6 +18,31 @@ public class Avion {
         this.to = to;
     }
 
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+
     @Override
     public String toString() {
         return String.format(
