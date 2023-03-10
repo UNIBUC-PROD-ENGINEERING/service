2ba0b994e0242fc843d4211edb422a9cc5211725
Giani2001 <gianivisalon@yahoo.com>
2023-03-10 04:41:02 +0200

    add-client-service

---
+++ src/main/java/ro/unibuc/hello/service/ClientService.java
@@ -0,0 +1,79 @@
+package ro.unibuc.hello.service;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import ro.unibuc.hello.data.*;
+
+
+import ro.unibuc.hello.dto.ClientDTO;
+import ro.unibuc.hello.dto.MenuDTO;
+import ro.unibuc.hello.dto.OrderDTO;
+import ro.unibuc.hello.exception.EntityNotFoundException;
+
+import java.util.List;
+import java.util.ArrayList;
+
+@Component
+public class ClientService {
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public List<ClientDTO> getClients() {
+        ArrayList<ClientDTO> clientDTOS = new ArrayList<>();
+        clientRepository.findAll().forEach(clientEntity -> clientDTOS.add(new ClientDTO(clientEntity)));
+        return clientDTOS;
+    }
+
+    public ClientDTO getClient(String id) {
+        ClientEntity client = clientRepository.findById(String.valueOf(new ObjectId(id))).orElse(null);
+        if (client != null) {
+            return new ClientDTO(client);
+        } else {
+            return null;
+        }
+    }
+
+
+    public ClientDTO insertClient(String name, String email, String address, List<String> orderIds) {
+        ClientEntity client = new ClientEntity(name, email, address);
+        ArrayList<OrderEntity> orderEntities = new ArrayList<>();
+        if(orderIds != null && !orderIds.isEmpty())
+            orderIds.forEach(id -> orderEntities.add(orderRepository.findById(String.valueOf(new ObjectId(id))).orElse(null)));
+        else
+            client.setOrders(null);
+        if(!orderEntities.isEmpty() && orderEntities !=null)
+            client.setOrders(orderEntities);
+        return new ClientDTO(client);
+    }
+    public ClientDTO updateClient(String clientId,String name, String email, String address, List<String> orderIds){
+        ClientEntity client = clientRepository.findById(String.valueOf(new ObjectId(clientId))).orElse(null);
+        if(client != null){
+            if(name != null)
+                client.setName(name);
+            if(address != null)
+                client.setAddress(address);
+            if(email != null)
+                client.setEmail(email);
+            if(orderIds != null && !orderIds.isEmpty()){
+                ArrayList<OrderEntity> orderEntities = new ArrayList<>();
+                orderIds.forEach(id -> orderEntities.add(orderRepository.findById(String.valueOf(new ObjectId(id))).orElse(null)));
+                if(!orderEntities.isEmpty() && orderEntities !=null)
+                    client.setOrders(orderEntities);
+            }
+            else
+                client.setOrders(null);
+        return new ClientDTO(client);
+        }
+        else
+            return null;
+    }
+    public  String deleteClient(String id){
+        clientRepository.deleteById(String.valueOf(new ObjectId(id)));
+        return "Client with id:" + id + "was deleted";
+    }
+}
