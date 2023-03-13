39750af447d3a7cf7c1c6b5b3a48a5a1df552d94
Alexandru Buzescu <alexbuzescu@yahoo.com>
2023-03-13 14:49:21 +0200

    added movies

--- src/main/java/ro/unibuc/hello/HelloApplication.java
+++ src/main/java/ro/unibuc/hello/HelloApplication.java
@@ -30,6 +30,21 @@ public class HelloApplication {
 		movieRepository.save(new MovieEntity("Avatar",
 				"Jake Sully lives with his newfound family formed on the extrasolar moon Pandora. Once a familiar threat returns to finish what was previously started, Jake must work with Neytiri and the army of the Na'vi race to protect their home.",
 				192));
+		movieRepository.save(new MovieEntity("Ford v Ferrari",
+				"American car designer Carroll Shelby and driver Ken Miles battle corporate interference and the laws of physics to build a revolutionary race car for Ford in order to defeat Ferrari at the 24 Hours of Le Mans in 1966",
+				152));
+		movieRepository.save(new MovieEntity("Titanic",
+				"A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.",
+				196));
+		movieRepository.save(new MovieEntity("Dunkirk",
+				"Allied soldiers from Belgium, the British Commonwealth and Empire, and France are surrounded by the German Army and evacuated during a fierce battle in World War II.",
+				106));
+		movieRepository.save(new MovieEntity("Fight Club",
+				"An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more.",
+				139));
+		movieRepository.save(new MovieEntity("Bullet Train",
+				"Five assassins aboard a swiftly-moving bullet train find out that their missions have something in common."
+				,126));
 //		informationRepository.deleteAll();
 //		informationRepository.save(new InformationEntity("Overview",
 //				"This is an example of using a data storage engine running separately from our applications server"));
