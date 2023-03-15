7775d1cc2801786efb2e4cd59b14c1ea3ebfa529
yanastany <anastan0205@gmail.com>
2023-03-15 18:10:31 +0200

    Farmacist-Ana

--- src/main/java/ro/unibuc/hello/HelloApplication.java
+++ src/main/java/ro/unibuc/hello/HelloApplication.java
@@ -6,6 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import ro.unibuc.hello.data.InformationEntity;
 import ro.unibuc.hello.data.InformationRepository;
+<<<<<<< Updated upstream
+=======
+import ro.unibuc.hello.data.MedicamentEntity;
+import ro.unibuc.hello.data.MedicamentRepository;
+import ro.unibuc.hello.dto.Medicament;
+import ro.unibuc.hello.data.FarmacistEntity;
+import ro.unibuc.hello.data.FarmacistRepository;
+import ro.unibuc.hello.dto.Farmacist;
+>>>>>>> Stashed changes
 
 import javax.annotation.PostConstruct;
 
@@ -15,6 +24,14 @@ public class HelloApplication {
 
 	@Autowired
 	private InformationRepository informationRepository;
+<<<<<<< Updated upstream
+=======
+	@Autowired
+	private MedicamentRepository medicamentRepository;
+	@Autowired
+	private FarmacistRepository farmacistRepository;
+
+>>>>>>> Stashed changes
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
@@ -24,7 +41,17 @@ public class HelloApplication {
 	public void runAfterObjectCreated() {
 		informationRepository.deleteAll();
 		informationRepository.save(new InformationEntity("Overview",
+<<<<<<< Updated upstream
 				"This is an example of using a data storage engine running separately from our applications server"));
+=======
+				"This is an example of using a data storage engine running separately from our applications server")
+				);
+		medicamentRepository.deleteAll();
+		medicamentRepository.save(new MedicamentEntity("Medicamente",new ArrayList<Medicament>()));
+		farmacistRepository.deleteAll();
+		farmacistRepository.save(new FarmacistEntity("Farmacisti",new ArrayList<Farmacist>()));
+
+>>>>>>> Stashed changes
 	}
 
 }
