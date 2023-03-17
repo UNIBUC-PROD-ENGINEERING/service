db447342e138596de541fafc0375ec277209b09f
VladCalo <80626362+VladCalo@users.noreply.github.com>
2023-03-17 06:50:26 +0000

    small refactoring

--- src/main/java/ro/unibuc/hello/data/StudentRepository.java
+++ src/main/java/ro/unibuc/hello/data/StudentRepository.java
@@ -2,5 +2,6 @@ package ro.unibuc.hello.data;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+@Repository
 public interface StudentRepository extends MongoRepository<Student, String> {
 }
