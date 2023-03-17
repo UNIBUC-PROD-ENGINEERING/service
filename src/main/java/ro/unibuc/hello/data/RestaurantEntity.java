ab5df25178b4669929f90296c9d8d2e8eebb5f6f
Giani2001 <gianivisalon@yahoo.com>
2023-03-17 02:25:51 +0200

    update-restaurant-entity

--- src/main/java/ro/unibuc/hello/data/RestaurantEntity.java
+++ src/main/java/ro/unibuc/hello/data/RestaurantEntity.java
@@ -31,12 +31,14 @@ public class RestaurantEntity {
     }
 
     public RestaurantEntity(){}
-    public RestaurantEntity(String id, String name, String email, String address) {
+    public RestaurantEntity(String id, String name, String email, String address, ArrayList<OrderEntity> orders) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.address = address;
     }
+
+
 
     public String getId() {
         return id;
