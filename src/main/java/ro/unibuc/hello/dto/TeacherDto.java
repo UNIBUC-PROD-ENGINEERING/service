00a7dbcd0c5c33f88e50a8bd736d692ab815cbf2
Ivi123 <ivona_racasean9@yahoo.com>
2023-03-23 22:55:11 +0200

    Created test method for TeacherDto

--- src/main/java/ro/unibuc/hello/dto/TeacherDto.java
+++ src/main/java/ro/unibuc/hello/dto/TeacherDto.java
@@ -9,4 +9,10 @@ public class TeacherDto {
     private String firstName;
     private String lastName;
     private String subject;
+
+    public TeacherDto(String firstName, String lastName, String subject) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.subject = subject;
+    }
 }
