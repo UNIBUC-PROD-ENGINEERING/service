58f1b35780bb13a7bdfefdcd8e474e953e46c622
madap570 <madanico02@gmail.com>
2023-03-24 09:02:16 +0200

    [Unit Tests]Added unit testing for get categories

--- src/test/java/ro/unibuc/hello/controller/CategoryControllerTest.java
+++ src/test/java/ro/unibuc/hello/controller/CategoryControllerTest.java
@@ -52,4 +52,21 @@ public class CategoryControllerTest {
 
         Assertions.assertTrue(result.getResponse().getContentAsString().isEmpty());
     }
+
+    @Test
+    void test_deleteCategory() throws Exception{
+        CategoryDTO categoryDTO = CategoryDTO.builder()
+                .categoryName("Test Category").build();
+
+        doNothing().when(categoryService).deleteCategoryById("1");
+
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.delete("/categories/deleteCategory/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(categoryDTO));
+
+        MvcResult result = mockMvc.perform(mockRequest)
+                .andExpect(status().isOk()).andReturn();
+
+        Assertions.assertTrue(result.getResponse().getContentAsString().isEmpty());
+}
 }
