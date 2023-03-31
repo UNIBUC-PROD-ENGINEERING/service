55955480ceb74c719d6736c6f8a37ef4ce2c7d48
danielurs24 <danielursubalaci@gmail.com>
2023-03-31 09:55:31 +0300

    Update

---
+++ src/test/java/ro/unibuc/hello/dto/CarDTOTest.java
@@ -0,0 +1,12 @@
+package ro.unibuc.hello.dto;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ro.unibuc.hello.data.Car;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarDTOTest {
+
+
+}
