aa6bd5ec7d1274353aa828d4aa9a75e02017e137
Ivi123 <ivona_racasean9@yahoo.com>
2023-05-04 22:44:07 +0300

    Monitoring - UpdateTeacher endpoint

--- src/main/java/ro/unibuc/hello/controller/TeacherController.java
+++ src/main/java/ro/unibuc/hello/controller/TeacherController.java
@@ -1,5 +1,9 @@
 package ro.unibuc.hello.controller;
 
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,11 +18,16 @@ public class TeacherController {
 
     private final TeacherService teacherService;
 
+    @Autowired
+    MeterRegistry metricsRegistry;
+
     public TeacherController(TeacherService teacherService) {
         this.teacherService = teacherService;
     }
 
     @PatchMapping("/{teacherId}/teacher")
+    @Timed(value = "hello.updateteacher.time", description = "Time taken to update a teacher")
+    @Counted(value = "hello.updateteacher.count", description = "Times updateTeacher was returned")
     public ResponseEntity<TeacherEntity> updateTeacher(@RequestBody TeacherDto dto,
                                                        @PathVariable String teacherId) {
         return ResponseEntity.ok(teacherService.updateTeacherData(dto, teacherId));
