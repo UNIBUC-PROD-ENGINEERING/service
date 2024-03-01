804617d1dd15689f80ae0491b70f56dc21a70f69
Tudor Coman <tudor.coman@s.unibuc.ro>
2024-03-01 11:38:54 +0200

    Add Error response (#1)

---
+++ src/main/java/ro/unibuc/hello/controller/IndexController.java
@@ -0,0 +1,18 @@
+package ro.unibuc.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class IndexController implements ErrorController {
+    private final static String PATH = "/error";
+
+    @RequestMapping(PATH)
+    @ResponseBody
+    public String getErrorPath() {
+        return "No Mapping Found";
+    }
+}
