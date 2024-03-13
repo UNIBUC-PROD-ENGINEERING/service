8e5234d772a3f15a4f83316e12ba48897666d0ff
Irina Enescu <93870739+irinaenescu2002@users.noreply.github.com>
2024-03-13 12:20:34 +0000

    add author-update endpoint

--- src/main/java/ro/unibuc/hello/controller/AuthorController.java
+++ src/main/java/ro/unibuc/hello/controller/AuthorController.java
@@ -3,9 +3,7 @@ package ro.unibuc.hello.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import ro.unibuc.hello.data.AuthorEntity;
 import ro.unibuc.hello.service.AuthorService;
@@ -23,5 +21,14 @@ public class AuthorController {
         var newAuthor = authorService.saveAuthor(authorCreationRequestDto.getName(),
                 authorCreationRequestDto.getNationality());
         return ResponseEntity.ok(newAuthor);
+    }
+
+    @PatchMapping("/authors/{id}")
+    @ResponseBody
+    public ResponseEntity<AuthorEntity> updateAuthor(@PathVariable String id, 
+                                                     @RequestBody AuthorCreationRequestDto authorCreationRequestDto) {
+        var updatedAuthor = authorService.updateAuthor(id, authorCreationRequestDto.getName(),
+                                                       authorCreationRequestDto.getNationality());
+        return ResponseEntity.ok(updatedAuthor);
     }
 }
