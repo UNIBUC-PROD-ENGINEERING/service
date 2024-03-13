b4c687712ac4e5aa0d2bbc0867aa2c8c79781567
AnaMariaVoinea <92676726+Moozey@users.noreply.github.com>
2024-03-13 22:09:03 +0000

    added patch reader by id and get readers

--- src/main/java/ro/unibuc/hello/controller/ReaderController.java
+++ src/main/java/ro/unibuc/hello/controller/ReaderController.java
@@ -1,13 +1,17 @@
 package ro.unibuc.hello.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-
+import ro.unibuc.hello.data.AuthorEntity;
 import ro.unibuc.hello.data.ReaderEntity;
 import ro.unibuc.hello.service.ReaderService;
+import ro.unibuc.hello.dto.AuthorCreationRequestDto;
+import ro.unibuc.hello.dto.Greeting;
 import ro.unibuc.hello.dto.ReaderCreationRequestDto;
 
 @Controller
@@ -15,6 +19,12 @@ public class ReaderController {
 
     @Autowired
     private ReaderService readerService;
+
+    @GetMapping("/readers")
+    @ResponseBody
+    private List<ReaderEntity> getReaders(){
+        return readerService.getAllReaders();
+    }
 
     @PostMapping("/readers")
     @ResponseBody
@@ -26,6 +36,16 @@ public class ReaderController {
                 readerCreationRequestDto.getBirthDate(),
                 readerCreationRequestDto.getRegistrationDate());
         return ResponseEntity.ok(newReader);
+    }
+
+    @PatchMapping("/readers/{id}")
+    @ResponseBody
+    public ResponseEntity<ReaderEntity> updateReader(@PathVariable String id, 
+                                                     @RequestBody ReaderCreationRequestDto readerCreationRequestDto) {
+        var updatedReader = readerService.updateReader(id,
+                                readerCreationRequestDto.getEmail(),
+                                readerCreationRequestDto.getPhoneNumber());
+        return ResponseEntity.ok(updatedReader);
     }
 
 
--- src/main/java/ro/unibuc/hello/service/ReaderService.java
+++ src/main/java/ro/unibuc/hello/service/ReaderService.java
@@ -1,6 +1,9 @@
 package ro.unibuc.hello.service;
 
+import java.io.Reader;
 import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Service;
@@ -25,6 +28,20 @@ public class ReaderService {
         log.debug("Creating a new reader '{}' with nationality '{}'", name, nationality);
         var readerEntity = ReaderEntity.builder().name(name).nationality(nationality).email(email).phoneNumber(phoneNumber).birthDate(birthDate).registrationDate(registrationDate).build();
         return readerRepository.save(readerEntity);
+    }
+
+    public List<ReaderEntity> getAllReaders() {
+        log.debug("Getting all readers");   
+        return readerRepository.findAll();
+        
+    }
+
+    public ReaderEntity updateReader(String id, String email, String phoneNumber) {
+        log.debug("Updating the reader with id '{}', setting email '{}' and phone '{}'", id, email, phoneNumber);
+        var reader = readerRepository.findById(id).get();
+        reader.setEmail(email);
+        reader.setPhoneNumber(phoneNumber);
+        return readerRepository.save(reader);
     }
 
 }
