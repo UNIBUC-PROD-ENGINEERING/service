780cf28062a47537a5438c0caa3549dbbc8359b2
KALAS908 <94225252+KALAS908@users.noreply.github.com>
2024-03-14 09:42:45 +0000

    Fara Spatiu

---
+++ src/main/java/ro/unibuc/hello/data/UserEntity.java
@@ -0,0 +1,34 @@
+package ro.unibuc.hello.data;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+
+public class UserEntity {
+
+    @Id
+    public String id;
+    public String lastName;
+    public String firstName;
+    public Integer age;
+    public String userName;
+
+    public UserEntity() {
+    }
+
+    public UserEntity(String lastName, String firstName, Integer age, String userName) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.age = age;
+        this.userName = userName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "User[lastName='%s', firstName='%s', age='%s', userName='%s']",
+                id, lastName, firstName, age, userName);
+    }
+    
+}
