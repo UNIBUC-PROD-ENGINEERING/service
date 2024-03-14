eebbad69d9c5b536a4ff53e4b58d220d590e63ff
AnaMariaVoinea <92676726+Moozey@users.noreply.github.com>
2024-03-14 10:00:35 +0000

    changed registrationDate to LocalDate

--- src/main/java/ro/unibuc/hello/controller/ReaderController.java
+++ src/main/java/ro/unibuc/hello/controller/ReaderController.java
@@ -2,6 +2,7 @@ package ro.unibuc.hello.controller;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
