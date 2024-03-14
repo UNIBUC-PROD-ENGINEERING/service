25de422422da0c1a81c3c52305f1a454f6c22930
Alexu33 <95488524+Alexu33@users.noreply.github.com>
2024-03-14 09:34:36 +0000

    [REFACTOR] Add pacient into Programare

--- src/main/java/ro/unibuc/hello/HelloApplication.java
+++ src/main/java/ro/unibuc/hello/HelloApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import ro.unibuc.hello.data.PacientRepository;
 import ro.unibuc.hello.data.DoctorEntity;
 import ro.unibuc.hello.data.DoctorRepository;
 import ro.unibuc.hello.data.InformationEntity;
 import ro.unibuc.hello.data.InformationRepository;
 import ro.unibuc.hello.data.IntervalOrarEntity;
 import ro.unibuc.hello.data.IntervalOrarRepository;
+import ro.unibuc.hello.data.PacientEntity;
 import ro.unibuc.hello.data.ProgramareEntity;
 import ro.unibuc.hello.data.ProgramareRepository;
 
@@ -19,7 +21,7 @@ import java.time.LocalDate;
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackageClasses = {InformationRepository.class, DoctorRepository.class, IntervalOrarRepository.class, ProgramareRepository.class})
+@EnableMongoRepositories(basePackageClasses = {InformationRepository.class, DoctorRepository.class, IntervalOrarRepository.class, ProgramareRepository.class, PacientRepository.class})
 public class HelloApplication {
 	@Autowired
 	private InformationRepository informationRepository;
@@ -29,6 +31,9 @@ public class HelloApplication {
     private ProgramareRepository programareRepository;
     @Autowired
     private IntervalOrarRepository intervalOrarRepository;
+	@Autowired
+	private PacientRepository pacientRepository;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
@@ -49,8 +54,12 @@ public class HelloApplication {
         IntervalOrarEntity intervalOrar2 = new IntervalOrarEntity(9, 0);
         intervalOrarRepository.save(intervalOrar2);
 
+		pacientRepository.deleteAll();
+		PacientEntity pacient1 = new PacientEntity("Bobita Stefan");
+		pacientRepository.save(pacient1);
+
 		programareRepository.deleteAll();
-        programareRepository.save(new ProgramareEntity(intervalOrar1, doctor1, LocalDate.now().plusDays(10)));
-        programareRepository.save(new ProgramareEntity(intervalOrar2, doctor1, LocalDate.now().plusDays(10)));	
+        programareRepository.save(new ProgramareEntity(intervalOrar1, doctor1, pacient1, LocalDate.now().plusDays(10)));
+        programareRepository.save(new ProgramareEntity(intervalOrar2, doctor1, pacient1, LocalDate.now().plusDays(10)));	
 	}
 }
