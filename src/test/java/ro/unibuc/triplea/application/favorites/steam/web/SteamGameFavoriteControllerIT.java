d43c72ebead0bb8b524d514aab248dbc2c05fe5c
Alexandru Urlateanu <82050014+alexandru-urlateanu@users.noreply.github.com>
2024-04-04 08:01:36 +0000

    (FEAT) Fixed failing integration test

--- src/test/java/ro/unibuc/triplea/application/favorites/steam/web/SteamGameFavoriteControllerIT.java
+++ src/test/java/ro/unibuc/triplea/application/favorites/steam/web/SteamGameFavoriteControllerIT.java
@@ -1,5 +1,6 @@
 package ro.unibuc.triplea.application.favorites.steam.web;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,10 +9,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import ro.unibuc.triplea.application.favorites.steam.dto.request.SteamGameFavoriteRequest;
 import ro.unibuc.triplea.application.favorites.steam.dto.response.SteamGameFavoriteResponse;
 import ro.unibuc.triplea.domain.favorites.steam.service.SteamGameFavoriteService;
@@ -20,19 +24,24 @@ import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 
 @Tag("IT")
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
-@AutoConfigureMockMvc
 public class SteamGameFavoriteControllerIT {
-
     @Autowired
+    private WebApplicationContext webApplicationContext;
     private MockMvc mockMvc;
 
     @MockBean // Mocking the service
     private SteamGameFavoriteService steamGameFavoriteService;
 
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).apply(springSecurity()).build();
+    }
+
+    @WithMockUser(username = "user", roles = "USER")
     @Test
     public void testAddFavorite() throws Exception {
         // Mocking the service method to return a fixed response
