7ad8d4285636c16907b226b40506de452d41e601
andreim16 <andreim16@yahoo.com>
2025-01-21 10:18:15 +0200

    Bump all infrastructure components

--- pre-start-monitoring.sh
+++ pre-start-monitoring.sh
@@ -4,4 +4,4 @@ set -x
 mkdir -p infrastructure/prometheus/durable infrastructure/loki/durable
 chmod a+w infrastructure/prometheus/durable infrastructure/loki/durable
 
-docker plugin install grafana/loki-docker-driver:2.4.1 --alias loki --grant-all-permissions
+docker plugin install grafana/loki-docker-driver:3.3.2 --alias loki --grant-all-permissions
