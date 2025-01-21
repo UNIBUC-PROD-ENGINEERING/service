f2d92ee7624f35f1569b0d1da3e7d4f45cde3b23
andreim16 <andreim16@yahoo.com>
2025-01-21 11:20:59 +0200

    Add volume for Jenkins config

--- pre-start-monitoring.sh
+++ pre-start-monitoring.sh
@@ -4,4 +4,4 @@ set -x
 mkdir -p infrastructure/prometheus/durable infrastructure/loki/durable
 chmod a+w infrastructure/prometheus/durable infrastructure/loki/durable
 
-docker plugin install grafana/loki-docker-driver:3.3.2 --alias loki --grant-all-permissions
+docker plugin install grafana/loki-docker-driver:3.3.2-amd64 --alias loki --grant-all-permissions
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -x
 
+mkdir -p /workspaces/jenkins_config
 docker compose --profile mongo --profile hello-service up -d 
