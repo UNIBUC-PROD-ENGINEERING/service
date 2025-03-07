afd329ba447c422c89d23a05d8472c7243eb1cde
Daria Horga <115492333+dariahorga@users.noreply.github.com>
2025-03-07 18:01:31 +0000

    users service added

---
+++ src/main/java/ro/unibuc/hello/service/UsersService.java
@@ -0,0 +1,49 @@
+package ro.unibuc.hello.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ro.unibuc.hello.data.User;
+import ro.unibuc.hello.data.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsersService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(String id) {
+        return userRepository.findById(id);
+    }
+
+    public User updateUser(String id, User updatedUser) {
+        if (userRepository.existsById(id)) {
+            updatedUser.setId(id);
+            return userRepository.save(updatedUser);
+        }
+        return null;
+    }
+
+    public boolean deleteUser(String id) {
+        if (userRepository.existsById(id)) {
+            userRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
+
