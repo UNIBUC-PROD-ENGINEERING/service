3918115bd85ccf596e992b09dc755ec1e09a6df6
Daria-Lapadus <127118850+darialapadus@users.noreply.github.com>
2025-03-10 08:26:28 +0000

    add user model

--- src/main/java/ro/unibuc/hello/model/User.java
+++ src/main/java/ro/unibuc/hello/model/User.java
@@ -1,3 +1,24 @@
+package main.java.ro.unibuc.hello.model;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="users")
 public class User {
-    
+    @Id
+    @SequenceGenerator(
+            name="user_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            generator = "user_sequence",
+            strategy = GenerationType.SEQUENCE
+    )
+    private int id;
+    private String email;
+    private String last_name;
+    private String first_name;
+    private String password;
 }
