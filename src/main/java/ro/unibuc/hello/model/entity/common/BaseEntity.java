1476c72a10feaa82a0648f08dfd92c592e61c7f8
Stefan Zaharie <stefan@zaha.tech>
2025-03-11 16:31:31 +0200

    feat: add postgres and base jpa entity

---
+++ src/main/java/ro/unibuc/hello/model/entity/common/BaseEntity.java
@@ -0,0 +1,28 @@
+package ro.unibuc.hello.model.entity.common;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
