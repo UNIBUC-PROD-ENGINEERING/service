9d6dd5b710fbfbc420fd290da185b9e444a897fa
Daria Horga <115492333+dariahorga@users.noreply.github.com>
2025-03-11 10:44:50 +0000

    update

--- src/main/java/ro/unibuc/hello/controller/UserController.java
+++ src/main/java/ro/unibuc/hello/controller/UserController.java
@@ -1,8 +1,6 @@
 package ro.unibuc.hello.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ro.unibuc.hello.data.User;
 import ro.unibuc.hello.service.UsersService;
@@ -18,45 +16,34 @@ public class UserController {
     private UsersService usersService;
 
     @PostMapping
-    public ResponseEntity<User> createUser(@RequestBody User user) {
-        User createdUser = usersService.createUser(user);
-        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    public User createUser(@RequestBody User user) {
+        return usersService.createUser(user);
     }
 
     @GetMapping
-    public ResponseEntity<List<User>> getAllUsers() {
-        List<User> users = usersService.getAllUsers();
-        return new ResponseEntity<>(users, HttpStatus.OK);
+    public List<User> getAllUsers() {
+        return usersService.getAllUsers();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable String id) {
+    public User getUserById(@PathVariable String id) {
         Optional<User> user = usersService.getUserById(id);
-        return user.map(u -> new ResponseEntity<>(u, HttpStatus.OK))
-                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+        return user.orElse(null); // Return null if not found, Spring will send 404
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<User> updateUser(@PathVariable String id, @RequestBody User user) {
-        User updatedUser = usersService.updateUser(id, user);
-        return updatedUser != null
-                ? new ResponseEntity<>(updatedUser, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public User updateUser(@PathVariable String id, @RequestBody User user) {
+        return usersService.updateUser(id, user);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteUser(@PathVariable String id) {
+    public String deleteUser(@PathVariable String id) {
         boolean isDeleted = usersService.deleteUser(id);
-        return isDeleted
-                ? new ResponseEntity<>("User deleted", HttpStatus.NO_CONTENT)
-                : new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
+        return isDeleted ? "User deleted" : "User not found";
     }
 
     @GetMapping("/email/{email}")
-    public ResponseEntity<User> getUserByEmail(@PathVariable String email) {
-        User user = usersService.getUserByEmail(email);
-        return user != null
-                ? new ResponseEntity<>(user, HttpStatus.OK)
-                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    public User getUserByEmail(@PathVariable String email) {
+        return usersService.getUserByEmail(email);
     }
 }
