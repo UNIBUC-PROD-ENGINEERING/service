81a37214d8dc8fb060fbc9c227516e01bfe7761a
Michael-2511 <116028853+Michael-2511@users.noreply.github.com>
2025-03-12 07:31:21 +0000

    Added Subscription Controller, Service, Repository

--- src/main/java/ro/unibuc/hello/controller/SubscriptionController.java
+++ src/main/java/ro/unibuc/hello/controller/SubscriptionController.java
@@ -6,27 +6,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import ro.unibuc.hello.dto.Game;
+// import ro.unibuc.hello.dto.Game;
+import ro.unibuc.hello.dto.Subscription;
 import ro.unibuc.hello.exception.EntityNotFoundException;
-import ro.unibuc.hello.service.GamesService;
+// import ro.unibuc.hello.service.GamesService;
+import ro.unibuc.hello.service.SubscriptionService;
 import ro.unibuc.hello.service.GreetingsService;
 
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-public class GamesController {
-
+public class SubscriptionController {
+    
     @Autowired
-    private GamesService gamesService;
+    private SubscriptionService subscriptionsService;
 
-
-    @GetMapping("/games")
+    @GetMapping("/subscriptions")
     @ResponseBody
-    public List<Game> getAllGames() {
-        return gamesService.getAllGames();
+    public List<Subscription> getAllSubscriptions() {
+        return subscriptionsService.getAllSubscriptions();
     }
 
 
 }
-
---
+++ src/main/java/ro/unibuc/hello/service/SubscriptionsService.java
@@ -0,0 +1,30 @@
+package ro.unibuc.hello.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+// import ro.unibuc.hello.data.GameEntity;
+// import ro.unibuc.hello.data.GameRepository;
+// import ro.unibuc.hello.dto.Game;
+import ro.unibuc.hello.data.SubscriptionEntity;
+import ro.unibuc.hello.data.SubscriptionRepository;
+import ro.unibuc.hello.data.Subscription;
+
+import ro.unibuc.hello.exception.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Component
+public class SubscriptionsService {
+    @Autowired
+    private SubscriptionRepository subscriptionRepository;
+
+    public List<Subscription> getAllSubscriptions() {
+        List<SubscriptionEntity> entities = subscriptionRepository.findAll();
+        return entities.stream()
+            .map(entity -> new Subscription(entity.getTier(), entity.getPrice()))
+            .collect(Collectors.toList());
+    }
+}
