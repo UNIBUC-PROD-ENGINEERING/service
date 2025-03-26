dd76b4a764cf92cb3e2d9622753ed85156677f99
Ioan-Tudor-Alexandru Anghel <ioan.anghel123@gmail.com>
2025-03-26 08:18:42 +0200

    Added modification from fork source, since conflicts had to be resolved for sync

--- src/test/java/ro/unibuc/hello/controller/GreetingsControllerIntegrationTest.java
+++ src/test/java/ro/unibuc/hello/controller/GreetingsControllerIntegrationTest.java
@@ -36,10 +36,7 @@ public class GreetingsControllerIntegrationTest {
     @Container
     public static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:6.0.20")
             .withExposedPorts(27017)
-            .withEnv("MONGO_INITDB_ROOT_USERNAME","root") // user
-            .withEnv("MONGO_INITDB_ROOT_PASSWORD", "example") // password
-            .withEnv("MONGO_INITDB_DATABASE", "testdb") // dbname
-            .withCommand("--auth");
+            .withSharding();
 
     @BeforeAll
     public static void setUp() {
@@ -53,7 +50,7 @@ public class GreetingsControllerIntegrationTest {
 
     @DynamicPropertySource
     static void setProperties(DynamicPropertyRegistry registry) {
-        final String MONGO_URL = "mongodb://root:example@localhost:";
+        final String MONGO_URL = "mongodb://localhost:";
         final String PORT = String.valueOf(mongoDBContainer.getMappedPort(27017));
 
         registry.add("mongodb.connection.url", () -> MONGO_URL + PORT);
