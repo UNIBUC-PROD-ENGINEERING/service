3ef1a63f2042d2c82e49f65627db1293894a69b5
AlexandruMiroiu1 <alexandru.miroiu@s.unibuc.ro>
2026-03-10 15:43:52 +0000

    Component Entity

--- src/main/java/ro/unibuc/prodeng/model/ComponentEntity.java
+++ src/main/java/ro/unibuc/prodeng/model/ComponentEntity.java
@@ -1,21 +1,43 @@
 package ro.unibuc.prodeng.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.Instant;
+import java.util.List;
+
+@Data // Generates getters, setters, to string, equals and hashcode
+@NoArgsConstructor //Generates the an empty constructor
 @Document(collection = "components")
-public class ComponentEntity(
+public class ComponentEntity {
+
     @Id
-    private String id,
-    private String name,
-    private String description,
-    private String category,
-    private List<String> photoUrls,
-    private int quantity,
-    private int availableQuantity,
-    private boolean isConsumable,
-    public List<String> tags,
-    public String infoMD,
-    public boolean active
-    
-)
+    private String id;
+    private String name;
+    private String description;
+    private String category;
+    private List<String> photoUrls;
+    private Integer quantity;
+    private Integer availableQuantity;
+    private Boolean isConsumable;
+    private List<String> tags;
+    private String infoMarkdown;
+    private Instant createdAt = Instant.now(); 
+
+
+    public void setQuantity(Integer quantity) {
+        if (quantity != null && this.availableQuantity != null && this.availableQuantity > quantity) {
+            throw new IllegalArgumentException("Total quantity cannot be set lower than the currently available quantity.");
+        }
+        this.quantity = quantity;
+    }
+
+    public void setAvailableQuantity(Integer availableQuantity) {
+        if (availableQuantity != null && this.quantity != null && availableQuantity > this.quantity) {
+            throw new IllegalArgumentException("Available quantity cannot exceed total quantity.");
+        }
+        this.availableQuantity = availableQuantity;
+    }
+}
