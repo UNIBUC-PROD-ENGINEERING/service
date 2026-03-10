521d5236e9e50876f42c0d99641299270a3d9574
octavalexandru <octav-andrei.alexandru@s.unibuc.ro>
2026-03-10 17:35:47 +0200

    data models

--- src/main/java/ro/unibuc/prodeng/model/UserEntity.java
+++ src/main/java/ro/unibuc/prodeng/model/UserEntity.java
@@ -2,7 +2,7 @@ package ro.unibuc.prodeng.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
+import lombok.Data;
 
 @Data
 @Document(collection = "users")
