68b57c57ef7138f39aa905fdeb51fcd329b4d7fe
AlexandruMiroiu1 <alexandru.miroiu@s.unibuc.ro>
2026-03-15 15:19:26 +0000

    Fix user entity

--- src/main/java/ro/unibuc/prodeng/model/UserEntity.java
+++ src/main/java/ro/unibuc/prodeng/model/UserEntity.java
@@ -2,16 +2,11 @@ package ro.unibuc.prodeng.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import lombok.Data;
 
-@Data
 @Document(collection = "users")
-public class UserEntity(
+public record UserEntity(
     @Id
-    private String id,
-    private String name,
-    private String email,
-    private String group,
-    private Boolean isAdmin
-
+    String id,
+    String name,
+    String email
 ) {}
