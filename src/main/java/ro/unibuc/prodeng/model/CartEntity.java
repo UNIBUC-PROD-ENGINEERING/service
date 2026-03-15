9f149f3dda292609b02a4cbe552f92edddbf7744
LightWings-stack <149331634+LightWings-stack@users.noreply.github.com>
2026-03-15 14:54:20 +0000

    update cart si service

--- src/main/java/ro/unibuc/prodeng/model/CartEntity.java
+++ src/main/java/ro/unibuc/prodeng/model/CartEntity.java
@@ -1,17 +1,23 @@
 package ro.unibuc.prodeng.model;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
+@Data
 @Document(collection = "cart")
-public class CartEntitiy(
+public class CartEntity {
+
     @Id
-    private String id,
-    private String userID,
-    private List<CartItems> items,
-    private CartStatus status,
-    private LocalDateTime createdAt
+    private String id;
+    private String userID;
+    private List<CartItem> items = new ArrayList<>();
+    private CartStatus status;
+    private Instant createdAt = Instant.now();
 
     public enum CartStatus {
         OPEN, SUBMITTED
@@ -21,5 +27,11 @@ public class CartEntitiy(
     public static class CartItem {
         private String componentId;
         private int quantity;
+        
+        public CartItem() {}
+        public CartItem(String componentId, int quantity) {
+            this.componentId = componentId;
+            this.quantity = quantity;
+        }
     }
-)
+}
